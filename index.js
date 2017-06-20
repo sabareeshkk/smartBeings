4e8b33c0d96d15e2e3562952b038c8065ad4465f
sabareeshk <sabareeshk1991@gmail.com>
2017-06-20 13:52:31 +0530

    commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose.connect(mongoDB);
 
 //Get the default connection
 var db = mongoose.connection;
-console.log(db);
+// console.log(db);
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
